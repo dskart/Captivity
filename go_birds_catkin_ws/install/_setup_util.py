d8cc3261f5630d33859ce72ba3ea987c1e283f52
Raphael <dskart11@gmail.com>
2019-10-14 20:52:41 -0400

    commit to change name

--- go_birds_catkin_ws/install/_setup_util.py
+++ go_birds_catkin_ws/install/_setup_util.py
@@ -270,7 +270,7 @@ if __name__ == '__main__':
 
         if not args.local:
             # environment at generation time
-            CMAKE_PREFIX_PATH = '/home/raphael/go_birds_catkin_ws/devel;/home/raphael/ros_catkin_ws/install_isolated'.split(';')
+            CMAKE_PREFIX_PATH = '/home/raphael/ros_catkin_ws/install_isolated'.split(';')
         else:
             # don't consider any other prefix path than this one
             CMAKE_PREFIX_PATH = []
--- go_birds_catkin_ws/install/include/go_birds/AddTwoIntsRequest.h
+++ go_birds_catkin_ws/install/include/go_birds/AddTwoIntsRequest.h
@@ -76,7 +76,7 @@ namespace message_traits
 
 
 // BOOLTRAITS {'IsFixedSize': True, 'IsMessage': True, 'HasHeader': False}
-// {'go_birds': ['/home/raphael/go_birds_catkin_ws/src/go_birds/msg'], 'std_msgs': ['/home/raphael/ros_catkin_ws/install_isolated/share/std_msgs/cmake/../msg']}
+// {'go_birds': ['/home/raphael/Documents/go_birds/go_birds_catkin_ws/src/go_birds/msg'], 'std_msgs': ['/home/raphael/ros_catkin_ws/install_isolated/share/std_msgs/cmake/../msg']}
 
 // !!!!!!!!!!! ['__class__', '__delattr__', '__dict__', '__doc__', '__eq__', '__format__', '__getattribute__', '__hash__', '__init__', '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__', '_parsed_fields', 'constants', 'fields', 'full_name', 'has_header', 'header_present', 'names', 'package', 'parsed_fields', 'short_name', 'text', 'types']
 
